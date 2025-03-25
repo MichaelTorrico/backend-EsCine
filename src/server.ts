ed84110494d6dd7391326ade1cf0ea45d16e995a
michael-torrico <michaeltorrico123@gmail.com>
2025-03-24 23:53:11 -0300

    estructura de archivos y tal

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import express from 'express'
 import { port } from './utils/config/EnvConfig'
 import movieRouter from './movie/infrastructure/movieRouter'
-
+import productRouter from './product/infrastructure/productRouter'
 const app = express()
 
 app.use(express.json())
 
 app.use('/movie', movieRouter)
+app.use('/product', productRouter)
+
 app.get('/', (req, res) => {
   res.send('<h1>www</h1>')
 })
